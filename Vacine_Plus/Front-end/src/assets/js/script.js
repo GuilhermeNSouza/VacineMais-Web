64a84a7b3ad31a4f7e373b9379e4a4dc85ff9d58
GuilhermeNSouza <guilhermenariodesouza@gmail.com>
2023-06-18 12:12:07 -0400

    pagina inicial completamente adaptada p/ o projeto integrador

--- Vacine_Plus/Front-end/src/assets/js/script.js
+++ Vacine_Plus/Front-end/src/assets/js/script.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     $(window).scroll(function(){
         var scroll = $(window).scrollTop();
         if (scroll > 120) {
-          $(".change-color-scroll").css("background" , "#1C3D32");
+          $(".change-color-scroll").css("background" , "#2a507f");
           $(".change-color-scroll").removeClass("bg-glassmorphim");
         }
   
